ff8e4da7654fe354fa1e8409961d2d82346554ff
Connor Scott <90429126+CScott17@users.noreply.github.com>
2023-09-30 11:17:57 -0400

    Update ticketsystem.py

--- ticketsystem.py
+++ ticketsystem.py
@@ -62,6 +62,10 @@ def nonadmin_page():
 @ui.page('/admin_page')
 def admin_page():
     ui.label("Admin")
+    ui.button("Logout", on_click=lambda: ui.open(login_page))
+    ui.button("View Created Tickets", on_click=lambda: ui.open(
+        admin_ticket_view_list))
+
 
 
 # Creating a ticket as a nonadmin page. Lets user type title and issue, and then when they submit,
@@ -179,6 +183,95 @@ def nonadmin_ticket_view_more_info():
     ui.label("Ticket Assignee: " + ticketAssignee.at[0, "Assignee"])
     ui.label("Ticket Status: " + ticketStatus.at[0, "Status"])
 
+
+# Admin is able to look at ticket title and number
+
+@ui.page("/admin_ticket_view_list")
+def admin_ticket_view_list():
+    ui.button("Go back", on_click=lambda: ui.open(admin_page))
+
+    global username
+
+    print("Username getting pulled is " + username)
+    df_tickets = pd.read_sql_query(
+        "SELECT TicketNumber,Title from Tickets WHERE User = '" + username + "'", db_tickets)
+
+    grid = ui.aggrid.from_pandas(df_tickets).classes('max-h-40')
+    grid.set_visibility(True)
+
+    ticketNumbersSqlQueryGet = pd.read_sql_query(
+        "SELECT TicketNumber FROM Tickets WHERE User = '" + username + "'", db_tickets)
+
+    # This creates a dropdown for the user to pick from which ticket they would like to view further information on.
+    arrayOfTicketNumbers = []
+    for i in range(0, len(ticketNumbersSqlQueryGet), 1):
+        print("One of " + str(username) + "'s Ticket Numbers: " +
+              str(ticketNumbersSqlQueryGet.at[i, 'TicketNumber']))
+        arrayOfTicketNumbers.append(
+            ticketNumbersSqlQueryGet.at[i, 'TicketNumber'])
+
+    global queriedTicketNumber
+    ui.label(
+        "Select the ticket from the dropdown to see more information on it.")
+    queriedTicketNumber = ui.select(options=arrayOfTicketNumbers,
+                                    on_change=lambda: ui.open(admin_ticket_view_info))
+    
+
+
+# Show initial details of the selected ticket similar to the nonadmin view
+
+@ui.page("/admin_ticket_view_info")
+def admin_ticket_view_info():
+    global queriedTicketNumber
+
+    print("Showing Ticket " + str(queriedTicketNumber.value) + ".")
+    ui.button("Go back", on_click=lambda: ui.open(admin_ticket_view_list))
+
+    ticketNumber = queriedTicketNumber.value
+
+    ticketTitle = pd.read_sql_query(
+        "SELECT Title from Tickets WHERE TicketNumber = '" + str(ticketNumber) + "'", db_tickets)
+
+    ticketDesc = pd.read_sql_query(
+        "SELECT Description from Tickets WHERE TicketNumber = '" + str(ticketNumber) + "'", db_tickets)
+
+    ui.label("Ticket #" + str(ticketNumber))
+    ui.label("Ticket Title: " + ticketTitle.at[0, "Title"])
+    ui.label("Ticket Description: " + ticketDesc.at[0, "Description"])
+
+    ui.button("View further information", on_click=lambda: ui.open(
+        admin_ticket_view_more_info))
+
+
+
+# Edit page to allow the admin to respond and update the status of a given ticket
+
+@ui.page("/admin_ticket_edit_page")
+def admin_ticket_edit_page():
+    ui.label("Edit Page")
+    ui.button("Save Button")
+
+# View more information about a selected ticket also similar to the nonadmin side
+
+@ui.page("/admin_ticket_view_more_info")
+def admin_ticket_view_more_info():
+    ui.button("Go back", on_click=lambda: ui.open(admin_ticket_view_info))
+
+    global queriedTicketNumber
+    ticketNumber = queriedTicketNumber.value
+
+    ticketTimestamp = pd.read_sql_query(
+        "SELECT Timestamp from Tickets WHERE TicketNumber = '" + str(ticketNumber) + "'", db_tickets)
+
+    ticketAssignee = pd.read_sql_query(
+        "SELECT Assignee from Tickets WHERE TicketNumber = '" + str(ticketNumber) + "'", db_tickets)
+
+    ticketStatus = pd.read_sql_query(
+        "SELECT Status from Tickets WHERE TicketNumber = '" + str(ticketNumber) + "'", db_tickets)
+
+    ui.label("Time Created: " + ticketTimestamp.at[0, "Timestamp"])
+    ui.label("Ticket Assignee: " + ticketAssignee.at[0, "Assignee"])
+    ui.label("Ticket Status: " + ticketStatus.at[0, "Status"])
 
 login_page()
 ui.run()
